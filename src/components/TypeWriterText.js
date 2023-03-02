94a9f3afd74b08c76406c3fc340fe943f1be756d
ethzhang <ethanzhang.cn@gmail.com>
2023-03-02 21:23:20 +0800

    responsive home

--- src/components/TypeWriterText.js
+++ src/components/TypeWriterText.js
@@ -23,8 +23,18 @@ span{
 .text-3 {
     color: red;
 }
-`
 
+@media (max-width: 70em) {
+    font-size: ${props => props.theme.fontxl};
+}
+@media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+}
+@media (max-width: 40em) {
+    width: 90%;
+}
+`
 
 const Subtitle = styled.h3`
 font-size: ${props => props.theme.fontlg};
@@ -34,11 +44,29 @@ font-weight: 600;
 margin-bottom: 1rem;
 width: 80%;
 align-self: flex-start;
+
+@media (max-width: 40em) {
+    font-size: ${props => props.theme.fontmd};
+}
+
+@media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+}
 `
 
 const ButtonContainer =styled.div`
 width: 80%;
 align-self: flex-start;
+
+@media (max-width: 48em) {
+    align-self: center;
+    text-align: center;
+
+    button{
+        margin: 0 auto ;
+    }
+} 
 `
 
 const TypeWriterText = () => {
@@ -46,7 +74,6 @@ const TypeWriterText = () => {
         <>
         <Title>
             Discover a new era of cool
-
             <Typewriter
                 options={{
                     autostart: true,
